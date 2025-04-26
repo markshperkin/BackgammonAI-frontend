1e153be3068c273857cc9db8d006545eaa61a122
mark shperkin <markshperkin1@gmail.com>
2025-04-26 11:43:25 -0400

    api for deploy

--- src/api.js
+++ src/api.js
@@ -1,13 +1,16 @@
 import axios from "axios";
 
-// const API_URL = "https://backgammonai-backend.onrender.com/api/game";
-const API_URL = "http://127.0.0.1:5000/api/game";
+const API_BASE = import.meta.env.VITE_API_URL;
+const SSE_BASE = import.meta.env.VITE_SSE_URL;
+// const API_BASE = "http://127.0.0.1:5000/api/game";
+// const SSE_BASE = "http://127.0.0.1:5000/"
+
 
 // start game
 export const startGame = async (aiType) => {
     try {
         const response = await axios.post(
-            `${API_URL}/start`,
+            `${API_BASE}/start`,
             { aiType }
         );
         return response.data;
@@ -20,7 +23,7 @@ export const startGame = async (aiType) => {
 // rolling the dice
 export const rollDice = async () => {
     try {
-        const response = await axios.get(`${API_URL}/roll-dice`);
+        const response = await axios.get(`${API_BASE}/roll-dice`);
         return response.data;
     } catch (error) {
         console.error("API Error:", error);
@@ -31,7 +34,7 @@ export const rollDice = async () => {
 // make a move
 export const makeMove = async (start, end) => {
     try {
-        const response = await axios.post(`${API_URL}/move`, { start, end });
+        const response = await axios.post(`${API_BASE}/move`, { start, end });
         return response.data;
     } catch (error) {
         console.error("API Error:", error);
@@ -42,7 +45,7 @@ export const makeMove = async (start, end) => {
 // get the game state
 export const getGameState = async () => {
     try {
-        const response = await axios.get(`${API_URL}/state`);
+        const response = await axios.get(`${API_BASE}/state`);
         return response.data;
     } catch (error) {
         console.error("API Error:", error);
@@ -53,7 +56,7 @@ export const getGameState = async () => {
 // get all valid moves
 export const getValidMoves = async (payload) => {
     try {
-      const response = await axios.post(`${API_URL}/valid-moves`, payload);
+      const response = await axios.post(`${API_BASE}/valid-moves`, payload);
       return response.data;
     } catch (error) {
       console.error("API Error:", error);
@@ -64,7 +67,7 @@ export const getValidMoves = async (payload) => {
 // AI agent move
 export const aiMove = async () => {
     try {
-      const response = await axios.post(`${API_URL}/ai-move`);
+      const response = await axios.post(`${API_BASE}/ai-move`);
       return response.data;
     } catch (error) {
       console.error("API Error:", error);
@@ -73,11 +76,11 @@ export const aiMove = async () => {
   };
   
 // data stream of minimax search graph
-export function connectSearchStream(url, onEvent) {
-    const source = new EventSource(url);
+export function connectSearchStream(SSE_BASE, onEvent) {
+    const source = new EventSource(SSE_BASE);
   
     source.onopen = () => {
-      console.log('[SSE ◀] Connected to', url);
+      console.log('[SSE Connected to server');
     };
   
     source.onmessage = (e) => {
